1f667eb1251e92a4d1fb287aa9d2e558863ebe9b
mgh3326 <mgh3326@naver.com>
2018-01-05 23:58:05 +0900

    gitpush

---
+++ mygcc.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        system("gcc");
+    }
+    else
+    {
+        int index = 0;
+        while (argv[1][index])
+        {
+            index++;
+        }
+        //char str2[index-2] = {0};
+        //char *arr = (char *)malloc(sizeof(char) *3+1+ index - 2+1+index);
+        char *arr = (char *)malloc(sizeof(char) * (10 + 2 * index));
+        //int oh_i = 0;
+        arr[0] = 'g';
+        arr[1] = 'c';
+        arr[2] = 'c';
+        arr[3] = ' ';
+        arr[4] = '-';
+        arr[5] = 'o';
+        arr[6] = ' ';
+        //arr="gcc ";
+        for (int i = 0; i < index - 2; i++)
+        {
+            arr[i + 7] = argv[1][i];
+        }
+        arr[5 + index] = '.';
+        arr[6 + index] = 'o';
+        arr[7 + index] = 'u';
+        arr[8 + index] = 't';
+        arr[9 + index] = ' ';
+        for (int i = 0; i < index; i++)
+        {
+            arr[10 + index + i] = argv[1][i];
+        }
+        printf("arr(%s)argv[1](%s)\n", arr, argv[1]);
+        system(arr);
+        free(arr);
+    }
+
+    return 0;
+}
